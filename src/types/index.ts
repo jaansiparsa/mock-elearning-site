b905ec7fd06293d5e42a57042df044056a7034cc
Jaansi Parsa <jaansi@jointaro.com>
2025-08-06 13:09:11 -0700

    http status codes

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,13 @@ export interface ApiResponse<T = unknown> {
   error?: string;
   message?: string;
 }
+
+export enum HTTPStatus {
+  OK = 200,
+  CREATED = 201,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+}
